2ca7d0173fb1ff6565e165f57a66c09e524052c4
Suhas S Murthy <murthy.suhas@gmail.com>
2025-03-06 21:57:58 +0530

    component unit testing

--- src/packaging-list/packing-list.test.tsx
+++ src/packaging-list/packing-list.test.tsx
@@ -1,5 +1,5 @@
 import PackingList from '.';
-import { it } from 'vitest';
+import { expect, it } from 'vitest';
 import { render, screen } from '../../test/utilities';
 
 it('renders the Packing List application', () => {
@@ -11,19 +11,54 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText('New Item Name');
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {}
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewButton = screen.getByRole('button', { name: 'Add New Item' });
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {}
-);
+  expect(newItemInput).toHaveValue('');
+  expect(addNewButton).toBeDisabled();
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {}
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewButton = screen.getByRole('button', { name: 'Add New Item' });
+
+  await user.type(newItemInput, 'New Item');
+  expect(addNewButton).toBeEnabled();
+});
+
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewButton = screen.getByRole('button', { name: 'Add New Item' });
+
+  await user.type(newItemInput, 'Apple watch');
+  await user.click(addNewButton);
+
+  expect(screen.getByLabelText('Apple watch')).not.toBeChecked();
+});
+
+it('Remove an item', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewButton = screen.getByRole('button', { name: 'Add New Item' });
+
+  await user.type(newItemInput, 'Apple watch 10');
+  await user.click(addNewButton);
+
+  const item = screen.getByLabelText('Apple watch 10');
+  const removeButton = screen.getByRole('button', {
+    name: 'Remove Apple watch 10',
+  });
+
+  await user.click(removeButton);
+
+  expect(item).not.toBeInTheDocument();
+});
