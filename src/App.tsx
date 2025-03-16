f013d281b85e12aa398a2b84fed2908a84079500
Suhas S Murthy <murthy.suhas@gmail.com>
2025-03-16 16:58:38 +0530

    integration testing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Application from './packaging-list';
+import PokemonSearch from './pokemon-search';
 import TimeZone from './time-zone';
 
 function App() {
   return (
     <>
       {/* <Counter /> */}
-      <Application />
-      <TimeZone getTodos={true} />
+      {/* <Application />
+      <TimeZone getTodos={true} /> */}
+      <PokemonSearch />
     </>
   );
 }
