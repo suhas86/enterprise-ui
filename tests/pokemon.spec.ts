0e82117eb34d0cd309cfbf20d409d2061bb8396c
Suhas S Murthy <murthy.suhas@gmail.com>
2025-03-16 17:33:16 +0530

    playwright screeshot ignore on ci added

--- tests/pokemon.spec.ts
+++ tests/pokemon.spec.ts
@@ -24,5 +24,12 @@ test('it should match the screeshot', async ({ page }) => {
     await input.fill('pikachu');
     const pokemonSearch = page.getByTestId('pokemon-search');
     // maxDiffPixels is the maximum number of different pixels allowed in the screenshot
+    // This is useful to avoid flaky tests. 
+    // On local it generates windows image but on GitHub it generates Linux image. 
+    // So, the screenshot will be different.
+    // So we can either 
+    // Modify your GitHub Actions to update snapshots in CI: & download verify and commit
+    // ignore it in CI
+    if (process.env.CI) test.skip();
     await expect(pokemonSearch).toHaveScreenshot({ maxDiffPixels: 100 });
 });
