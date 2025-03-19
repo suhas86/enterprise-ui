3503faf2930a995616b2fa837230170a676183be
Suhas S Murthy <murthy.suhas@gmail.com>
2025-03-19 20:15:03 +0530

    Update exercise.test.ts

--- src/examples/expect-works/exercise.test.ts
+++ src/examples/expect-works/exercise.test.ts
@@ -12,137 +12,157 @@ import { KanbanBoard } from '../../lib/kanban-board';
  * toThrowError: https://vitest.dev/api/expect.html#tothrowerror
  */
 
-it(
-    'should pass if the two numbers would add up correctly in a language other than JavaScript',
-    () => {
-        expect(0.2 + 0.1).toBeCloseTo(0.3);
-    },
+it.todo(
+  'should pass if the two numbers would add up correctly in a language other than JavaScript',
+  () => {
+    expect(0.2 + 0.1).toEqual(0.3);
+  },
 );
 
 describe('createPerson', () => {
-    it('should create an instance of a person', () => {
-        const person = createPerson('Ada Lovelace');
-        expect(person).toBeInstanceOf(Person);
-    });
+  it.todo('should create an instance of a person', () => {
+    const person = createPerson('Ada Lovelace');
+    expect.hasAssertions();
+    // Verify that person is an instance of a Person.
+  });
 });
 
 describe('Kanban Board', () => {
-    it('should include "Backlog" in board.statuses', () => {
-        const board = new KanbanBoard('Things to Do');
-        expect.hasAssertions();
-        expect(board.statuses).toContain('Backlog');
-    });
+  it.todo('should include "Backlog" in board.statuses', () => {
+    const board = new KanbanBoard('Things to Do');
+    expect.hasAssertions();
+    // Verify that board.statuses contains "Backlog".
+  });
 
-    it('should *not* include "Bogus" in board.statuses', () => {
-        const board = new KanbanBoard('Things to Do');
-        expect(board.statuses).not.toContain('Bogus');
-    });
+  it.todo('should *not* include "Bogus" in board.statuses', () => {
+    const board = new KanbanBoard('Things to Do');
+    expect.hasAssertions();
+    // Verify that board.statuses does not contain "Bogus".
+  });
 
-    it(
-        'should include an added status in board.statuses using #addStatus',
-        () => {
-            const board = new KanbanBoard('Things to Do');
-            board.addStatus('New Status');
-            expect(board.statuses).toContain('New Status');
-        },
-    );
+  it.todo(
+    'should include an added status in board.statuses using #addStatus',
+    () => {
+      const board = new KanbanBoard('Things to Do');
+      expect.hasAssertions();
+      // Use board.addStatus to add a status.
+      // Verify that the new status is—in fact—now in board.statuses.
+    },
+  );
 
-    it('should remove a status using #removeStatus', () => {
-        const board = new KanbanBoard('Things to Do');
-        expect.hasAssertions();
-        board.removeStatus('Backlog');
-        expect(board.statuses).not.toContain('Backlog');
-    });
+  it.todo('should remove a status using #removeStatus', () => {
+    const board = new KanbanBoard('Things to Do');
+    expect.hasAssertions();
+    // Use board.removeStatus to remove a status.
+
+    // You can be clever or you can just assume "Backlog" is in board.statuses
+    // by default.
+
+    // Verify that the status is no longer in in board.statuses.
+  });
 });
 
 describe('Person', () => {
-    it('will create a person with a first name', () => {
-        const person = new Person('Madonna');
-        expect(person.firstName).toBe('Madonna');
-    });
+  it.todo('will create a person with a first name', () => {
+    const person = new Person('Madonna');
+    expect.hasAssertions();
+    // Verify that person.firstName is correct.
+  });
 
-    it('will create a person with a first and last name', () => {
-        const person = new Person('Madonna Cicone');
-        expect(person.firstName).toBe('Madonna');
-        expect(person.lastName).toBe('Cicone');
-    });
+  it.todo('will create a person with a first and last name', () => {
+    const person = new Person('Madonna Cicone');
+    expect.hasAssertions();
+    // Verify that person.lastName is correct.
+  });
 
-    it('will create a person with a first, middle, and last name', () => {
-        const person = new Person('Madonna Louise Cicone');
-        expect(person.middleName).toBe('Louise');
-    });
+  it.todo('will create a person with a first, middle, and last name', () => {
+    const person = new Person('Madonna Louise Cicone');
+    expect.hasAssertions();
+    // Verify that person.middleName is correct.
+  });
 
-    it('will throw if you provide an empty string', () => {
-        const fn = () => {
-            new Person('');
-        };
-        expect(fn).toThrow();
-    });
+  it.todo('will throw if you provide an empty string', () => {
+    const fn = () => {
+      new Person('');
+    };
 
-    it(
-        'will throw a specific error message if you provide an empty string',
-        () => {
-            const errorMessage = 'fullName cannot be an empty string';
+    expect.hasAssertions();
 
-            const fn = () => {
-                new Person('');
-            };
-            expect(fn).toThrowError(errorMessage);
-        },
-    );
+    // Verify that function above throws.
+  });
 
-    it('will add a friend', () => {
-        const john = new Person('John Lennon');
-        const paul = new Person('Paul McCartney');
+  it.todo(
+    'will throw a specific error message if you provide an empty string',
+    () => {
+      const errorMessage = 'fullName cannot be an empty string';
 
-        john.addFriend(paul);
-        expect(john.friends.has(paul)).toBe(true);
+      const fn = () => {
+        new Person('');
+      };
 
-    });
+      expect.hasAssertions();
 
-    it('will mutually add a friend', () => {
-        const john = new Person('John Lennon');
-        const paul = new Person('Paul McCartney');
+      // Verify that function above throws the error message above.
+    },
+  );
 
-        john.addFriend(paul);
+  it.todo('will add a friend', () => {
+    const john = new Person('John Lennon');
+    const paul = new Person('Paul McCartney');
 
-        expect.hasAssertions();
-        expect(paul.friends.has(john)).toBe(true);
-    });
+    john.addFriend(paul);
 
-    it('will remove a friend', () => {
-        const john = new Person('John Lennon');
-        const paul = new Person('Paul McCartney');
+    expect.hasAssertions();
 
-        john.addFriend(paul);
-        john.removeFriend(paul);
+    // Verify that john.friends contains paul.
+  });
 
-        expect.hasAssertions();
-        expect(john.friends.has(paul)).toBe(false);
-    });
+  it.todo('will mutually add a friend', () => {
+    const john = new Person('John Lennon');
+    const paul = new Person('Paul McCartney');
 
-    it('will mutually remove friends', () => {
-        const john = new Person('John Lennon');
-        const paul = new Person('Paul McCartney');
+    john.addFriend(paul);
 
-        john.addFriend(paul);
-        john.removeFriend(paul);
+    expect.hasAssertions();
 
-        expect.hasAssertions();
-        expect(paul.friends.has(john)).toBe(false);
-    });
+    // Verify that paul.friends contains john.
+  });
+
+  it.todo('will remove a friend', () => {
+    const john = new Person('John Lennon');
+    const paul = new Person('Paul McCartney');
+
+    john.addFriend(paul);
+    john.removeFriend(paul);
+
+    expect.hasAssertions();
+
+    // Verify that john.friends does not include paul.
+  });
+
+  it.todo('will mutually remove friends', () => {
+    const john = new Person('John Lennon');
+    const paul = new Person('Paul McCartney');
+
+    john.addFriend(paul);
+    john.removeFriend(paul);
+
+    expect.hasAssertions();
+
+    // Verify that paul.friends does not include john.
+  });
 });
 
 const explode = () => {
-    throw new Error('Something went terribly wrong');
+  throw new Error('Something went terribly wrong');
 };
 
 describe('explode', () => {
-    it('should throw an error', () => {
-        expect(() => explode()).toThrow();
-    });
+  it.todo('should throw an error', () => {
+    explode();
+  });
 
-    it('should throw a specific error containing "terribly wrong"', () => {
-        expect(() => explode()).toThrowError('terribly wrong');
-    });
+  it.todo('should throw a specific error containing "terribly wrong"', () => {
+    explode();
+  });
 });
